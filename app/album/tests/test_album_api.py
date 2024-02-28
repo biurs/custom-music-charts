97e7104d89725c03417afdca9158b934a9a19ccc
Alex Grenier <agrenier243@gmail.com>
2024-02-28 10:04:50 -0800

    add mkdir to dockerfile

--- app/album/tests/test_album_api.py
+++ app/album/tests/test_album_api.py
@@ -486,7 +486,7 @@ class ImageUploadTests(TestCase):
         self.album.image.delete()
 
     def test_upload_image(self):
-        """Test uplaoding an image to a recipe."""
+        """Test uploading an image to a recipe."""
         url = image_upload_url(self.album.id)
         with tempfile.NamedTemporaryFile(suffix='.jpg') as image_file:
             img = Image.new('RGB', (10, 10))
